443950f3f26e118e7068019553a30215b0f6c423
Dipanshu <dipanshu.patel.wwl@gmail.com>
2025-08-28 13:27:22 +0530

    remove mongo from index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
 const faq = require('./routes/faq-Routes')
 const categoryRoutes = require("./routes/category-route");
@@ -29,7 +28,7 @@ const corsOptions = {
   },
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true // only if you're using cookies or sessions,
+  credentials: true
 };
 app.use(cors(corsOptions));
 
